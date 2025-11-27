c4e7fe2c66c610c6b5e835434816200088e25976
azeeee05 <azeemazm31@gmail.com>
2025-11-27 21:51:58 +0530

    fixed server issue

--- src/constant.ts
+++ src/constant.ts
@@ -1,10 +1,10 @@
-// const DEFAULT_API_BASE_URL = "http://localhost:4000/api/v1";
+// const DEFAULT_API_BASE_URL = "http://localhost:5000/api/v1";
 
 // export const API_BASE_URL =
 //   import.meta.env.VITE_API_BASE_URL ?? DEFAULT_API_BASE_URL;
 
 
-//production
+// production
 const DEFAULT_API_BASE_URL = "https://server.aesthco.com/api/v1";
   
 export const API_BASE_URL =
